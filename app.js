af8a3e4159096c6b63aea7103faf4ce652fbb9ef
Bryce DeSantis <potreader@hotmail.com>
2023-10-14 12:33:14 -0400

    Add button to display new book form

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+const myLibrary = []
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = false
-
-    this.info = function() {
-        if (read === false) {
-            return `${title} by ${author}, ${pages} pages, not read yet`
-        } else {
-            return `${title} by ${author}, ${pages} pages, read`
-        }
-    }
+    this.read = read
 }
+
+function addBookToLibrary() {
+
+}
+
+let newBookBtn = document.querySelector('.new-book-btn')
+
+newBookBtn.addEventListener('click', function() {
+    let bookForm = document.querySelector('.form-holder')
+    bookForm.style.display = 'block'
+})
