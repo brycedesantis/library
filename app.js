f3027f861e2a11eabd849aeffe3c5248c43466e8
Bryce DeSantis <potreader@hotmail.com>
2023-10-14 12:44:32 -0400

    Add functionality to add book to library

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#pages').value
+    let read = document.querySelector('#read').checked
 
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
 }
+
+let addBookBtn = document.querySelector('.add-book-btn')
+addBookBtn.addEventListener('click', function() {
+    event.preventDefault()
+    addBookToLibrary()
+})
 
 let newBookBtn = document.querySelector('.new-book-btn')
 
